df75763fea8bc952c1dce0ee64838bfc65d7dee3
rahusharma2312 <rahulsharma.nitian@gmail.com>
2024-12-02 23:41:14 -0500

    Update intro_to_ai_final_project.py

--- intro_to_ai_final_project.py
+++ intro_to_ai_final_project.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1Ds2RRWVGkoONkcAGuCjjS0heHaM5mLYz
 """
 
-pip install transformers torch datasets flask
-
 import random
 import json
 
